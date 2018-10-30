019d92dd144f10c92de5a9de365added3176c8e4
Jim Strother <jim.strother@gmail.com>
2018-10-30 18:08:33 -0500

    ran into issue with webpack not recognizing .scss files

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,21 +1,29 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    })
-  ]
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		})
+	]
 };
